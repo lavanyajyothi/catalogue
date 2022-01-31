33ee661ab9da9784a0af5e53f0bd3feba1ca8f91
lavanya <lavanya.bhadri>
2022-01-31 18:38:33 +0530

    catalogue

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -7,6 +7,7 @@ module "cart" {
   COMPONENT            = "catalogue"
   PORT                 = 8080
   IS_PRIVATE_LB        = true
+  LB_RULE_PRIORITY     = 101
   APP_VERSION          = var.APP_VERSION
   NEXUS_USER           = var.NEXUS_USER
   NEXUS_PASS           = var.NEXUS_PASS
